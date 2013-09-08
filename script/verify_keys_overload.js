3a3b257b4993f681801fb5226ec5146edab7b3c1
Zef Hemel <zef@zef.me>
2013-09-08 10:55:28 +0200

    More bugs related to #26

--- script/verify_keys_overload.js
+++ script/verify_keys_overload.js
@@ -1,31 +1,32 @@
 var fs = require('fs');
 
-var keys = JSON.parse(fs.readFileSync("../app/settings/keys.default.json"));
+var keys = JSON.parse(fs.readFileSync("app/settings/keys.default.json"));
 
 checkKeys("win");
 checkKeys("mac");
 
 function checkKeys(os) {
     var keyMap = {};
-    for(var k in keys) {
+
+    function registerAndVerify(key) {
+        //console.log(key);
+        if(os == "win" && key.indexOf("Command") !== -1) {
+            console.error("Found use of Command in Windows key: ", key);
+        }
+        if (keyMap[key]) {
+            console.error("Duplicate", key);
+        } else {
+            keyMap[key] = true;
+        }
+    }
+
+    for (var k in keys) {
         var key = keys[k];
         if (typeof key === "string") {
-            //console.log("Key", key);
-            if(keyMap[key]) {
-                console.log("Duplicate", key);
-            } else {
-                keyMap[key] = true;
-            }
-        } else if(key[os]) {
+            registerAndVerify(key);
+        } else if (key[os]) {
             var keyMappings = key[os].split("|");
-            keyMappings.forEach(function(keyMapping) {
-                //console.log("Key", keyMapping);
-                if(keyMap[keyMapping]) {
-                    console.log("Duplicate", keyMapping);
-                } else {
-                    keyMap[keyMapping] = true;
-                }
-            });
+            keyMappings.forEach(registerAndVerify);
         }
     }
 }
