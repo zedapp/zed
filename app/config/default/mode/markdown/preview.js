73e5d1adde3c1696a506c89ec5598a655e695784
Zef Hemel <zef@zef.me>
2014-02-02 10:40:07 +0100

    Fixes #71 by replacing showdown with pagedown.

--- app/config/default/mode/markdown/preview.js
+++ app/config/default/mode/markdown/preview.js
@@ -1,11 +1,11 @@
 define(function(require, exports, module) {
-    var showdown = require("configfs!./showdown.js");
+    var Markdown = require("configfs!./pagedown.js")
     var session = require("zed/session");
     var preview = require("zed/preview");
 
     return function(data, callback) {
         session.getText(data.path, function(err, text) {
-            var converter = new showdown.converter();
+            var converter = new Markdown.Converter();
             var html = converter.makeHtml(text);
             preview.showPreview(html, callback);
         });
