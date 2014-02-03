c22846f9805612fd5c059a037675e6508ff5f979
Zef Hemel <zef@zef.me>
2014-02-03 10:51:12 +0100

    Fixed #72 by adding a ctag indexer (detects classes and functions) and adding a built-in completer for all built PHP functions (and there are a lot of those).

--- app/config/default/mode/php/ctags.js
+++ app/config/default/mode/php/ctags.js
@@ -3,8 +3,7 @@ define(function(require, exports, module) {
     var session = require("zed/session");
     var ctags = require("zed/ctags");
 
-    var FN_REGEX = /function\s*\*?\s+([a-zA-Z0-9_\-\$]+)\s*\(/mg;
-    var PROP_FN_REGEX = /([a-zA-Z0-9_\-\$]+)\s*[:=]\s*function\s*\*?\s*\(/mg;
+    var FN_REGEX = /(class|function)\s+([a-zA-Z0-9_\$]+)\s*[\(\{]/mg;
     var indexToLine = require("zed/util").indexToLine;
 
     return function(data, callback) {
@@ -12,15 +11,10 @@ define(function(require, exports, module) {
         var tags = [];
         var path = data.path;
         session.getText(path, function(err, text) {
-            // Regular old functions
+            // Classes and functions
             while(match = FN_REGEX.exec(text)) {
                 //console.log(match);
-                tags.push({symbol: match[1], locator: indexToLine(text, match.index), path: path});
-            }
-            // Property functions
-            while(match = PROP_FN_REGEX.exec(text)) {
-                //console.log(match);
-                tags.push({symbol: match[1], locator: indexToLine(text, match.index), path: path});
+                tags.push({symbol: match[2], locator: indexToLine(text, match.index), path: path});
             }
             ctags.updateCTags(path, tags, callback);
         });
