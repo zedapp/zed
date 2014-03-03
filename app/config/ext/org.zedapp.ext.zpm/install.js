3c10656a7facd2382d660f0364288120a118e98f
Zef Hemel <zef@zef.me>
2014-03-03 15:22:11 +0100

    Fix install

--- app/config/ext/org.zedapp.ext.zpm/install.js
+++ app/config/ext/org.zedapp.ext.zpm/install.js
@@ -3,20 +3,20 @@ define(function(require, exports, module) {
     var zpm = require("configfs!./zpm.js");
     var project = require("zed/project");
 
-    return function(callback) {
+    return function(info, callback) {
        ui.prompt("URL to install extension from:", "", 400, 150, function(err, url) {
             if (url) {
                 zpm.install(url, function(err) {
-                    callback(err);
                     if (err) {
                         ui.prompt(err, undefined, 300, 150, function() {});
+                        callback(err);
                     } else {
                         project.isConfig(function(err, isConfig) {
                             if (isConfig) {
                                 project.reloadFileList();
                             }
                         });
-                        ui.prompt("Extension installed!", undefined, 300, 150, function() {});
+                        ui.prompt("Extension installed!", undefined, 300, 150, callback);
                     }
                 });
             }
