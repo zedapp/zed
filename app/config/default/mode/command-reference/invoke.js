f656a9382483b30dd94a0d77aa5498ebe2af4e3d
Robert Bruce Park <r@robru.ca>
2014-04-05 10:35:50 -0700

    Reduce clickable area to just the command title.

--- app/config/default/mode/command-reference/invoke.js
+++ app/config/default/mode/command-reference/invoke.js
@@ -4,18 +4,7 @@ var session = require("zed/session");
  * Inputs: lines, cursor
  */
 module.exports = function(info) {
-    var pos = info.inputs.cursor;
-    var lines = info.inputs.lines;
-
-    // Search backwards for the command title to invoke, aborting
-    // if we find a section header.
-    for (var i = pos.row; i > 1 && lines[i][0] !== ">"; --i) {
-        if (lines[i][0] === "#") {
-            return;
-        }
-    }
-
-    var command = lines[i + 2].match(/^   `(.*)`$/)[1];
+    var command = info.inputs.lines[info.inputs.cursor.row].match(/^   `(.*)`$/)[1];
     if (command) {
         session.callCommand(info.path, command);
     }
