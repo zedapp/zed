01870508d5795aefb64bc12d3297831e10bc5895
André König <andre.koenig@gmail.com>
2014-05-22 15:17:37 +0200

    Extended the HTTP API for performing PUT and DELETE requests.

--- app/js/sandbox/zed/http.js
+++ app/js/sandbox/zed/http.js
@@ -2,17 +2,38 @@
 define(function(require, exports, module) {
     var http_cache = require("../../lib/http_cache");
     
+    /**
+     * A request wrapper around the jQuery AJAX function.
+     * 
+     * options:
+     * 
+     *     headers: {Object}
+     *
+     *         The HTTP headers
+     * 
+     *     data: {Object || String} (optional)
+     * 
+     *          The HTTP request body payload.
+     *
+     *     type: {String} (optional; default = "Intelligent Guess")
+     * 
+     *          The type of data expected from the server.
+     * 
+     * @param {String} verb The HTTP verb (e.g. POST, PUT, etc.)
+     * @param {String} url The URL endpoint to which the request should be send.
+     * @param {Object} options The jQuery request configuration.
+     * 
+     */
     function request (verb, url, options) {
         var args = {};
         
         options = options || {};
-        
         args.url = url;
         args.type = verb;
         args.headers = options.headers;
         args.data = options.data;
         args.dataType = options.type;
-        
+
         return $.ajax(args);
     }
     
@@ -27,7 +48,9 @@ define(function(require, exports, module) {
 
             request('GET', url, options)
                 .done(function onDone (data, status, jqXHR) {
-                    callback(null, [data, jqXHR.status, jqXHR.getAllResponseHeaders()]);
+                    var payload = [data, jqXHR.status, jqXHR.getAllResponseHeaders()];
+
+                    callback(null, payload);
                 })
                 .fail(function onFail (jqXHR) {
                     callback(jqXHR.status)
@@ -36,7 +59,9 @@ define(function(require, exports, module) {
         post: function(url, options, callback) {
             request('POST', url, options)
                 .done(function onDone (data, status, jqXHR) {
-                    callback(null, [data, jqXHR.status, jqXHR.getAllResponseHeaders()]);
+                    var payload = [data, jqXHR.status, jqXHR.getAllResponseHeaders()];
+
+                    callback(null, payload);
                 })
                 .fail(function onFail (jqXHR) {
                     callback(jqXHR.status);
@@ -45,7 +70,9 @@ define(function(require, exports, module) {
         put: function(url, options, callback) {
              request('PUT', url, options)
                  .done(function onDone (data, status, jqXHR) {
-                     callback(null, [data, jqXHR.status, jqXHR.getAllResponseHeaders()]);
+                     var payload = [data, jqXHR.status, jqXHR.getAllResponseHeaders()];
+
+                     callback(null, payload);
                  })
                  .fail(function onFail (jqXHR) {
                      callback(jqXHR.status);
@@ -54,7 +81,9 @@ define(function(require, exports, module) {
          del: function(url, options, callback) {
               request('DELETE', url, options)
                   .done(function onDone (data, status, jqXHR) {
-                      callback(null, [data, jqXHR.status, jqXHR.getAllResponseHeaders()]);
+                      var payload = [data, jqXHR.status, jqXHR.getAllResponseHeaders()];
+                      
+                      callback(null, payload);
                   })
                   .fail(function onFail (jqXHR) {
                       callback(jqXHR.status);
