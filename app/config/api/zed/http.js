1079402bb36afb5340fab99c6c1f9eab08359829
Zef Hemel <zef@zef.me>
2014-08-11 10:31:27 +0200

    Exposed web server functionality via sandbox API and node-webkit support (via express).

--- app/config/api/zed/http.js
+++ app/config/api/zed/http.js
@@ -1,15 +1,15 @@
 /* global sandboxRequest*/
 module.exports = {
-    
+
     /**
      * HTTP API
-     * 
+     *
      * Provides functions for performing HTTP requests.
-     * 
+     *
      * Usage example:
-     * 
+     *
      *     var http = require('zed/http');
-     * 
+     *
      *     http.post('http://host.tld', {
      *         headers: {},
      *         data: {},
@@ -21,7 +21,7 @@ module.exports = {
      *     }, function onFail (status) {
      *
      *     });
-     * 
+     *
      */
     fetchUrl: function(url) {
         return sandboxRequest("zed/http", "fetchUrl", [url]);
@@ -29,10 +29,10 @@ module.exports = {
 
     /**
      * Sends a HTTP GET request to the given URL.
-     * 
+     *
      * @param {String} url The respective endpoint.
      * @param {Object} type The type of data expected from the server.
-     * 
+     *
      */
     get: function(url, type) {
         return sandboxRequest("zed/http", "get", [url, type]);
@@ -40,24 +40,24 @@ module.exports = {
 
     /**
      * Sends a HTTP POST request to the given URL.
-     * 
+     *
      * options:
-     * 
+     *
      *     headers: {Object}
      *
      *         The HTTP headers
-     * 
+     *
      *     data: {Object || String} (optional)
-     * 
+     *
      *          The HTTP request body payload.
      *
      *     type: {String} (optional; default = Intelligent Guess)
-     * 
+     *
      *          The type of data expected from the server.
-     * 
+     *
      * @param {String} url The respective endpoint.
      * @param {Object} options The configuration object.
-     * 
+     *
      */
     post: function(url, options) {
         return sandboxRequest("zed/http", "post", [url, options]);
@@ -65,24 +65,24 @@ module.exports = {
 
     /**
      * Sends a HTTP PUT request to the given URL.
-     * 
+     *
      * options:
-     * 
+     *
      *     headers: {Object}
      *
      *         The HTTP headers
-     * 
+     *
      *     data: {Object || String} (optional)
-     * 
+     *
      *          The HTTP request body payload.
      *
      *     type: {String} (optional; default = Intelligent Guess)
-     * 
+     *
      *          The type of data expected from the server.
-     * 
+     *
      * @param {String} url The respective endpoint.
      * @param {Object} options The configuration object.
-     * 
+     *
      */
     put: function(url, options) {
         return sandboxRequest("zed/http", "put", [url, options]);
@@ -90,26 +90,34 @@ module.exports = {
 
     /**
      * Sends a HTTP DELETE request to the given URL.
-     * 
+     *
      * options:
-     * 
+     *
      *     headers: {Object}
      *
      *         The HTTP headers
-     * 
+     *
      *     data: {Object || String} (optional)
-     * 
+     *
      *          The HTTP request body payload.
      *
      *     type: {String} (optional; default = Intelligent Guess)
-     * 
+     *
      *          The type of data expected from the server.
-     * 
+     *
      * @param {String} url The respective endpoint.
      * @param {Object} options The configuration object.
-     * 
+     *
      */
     del: function(url, options) {
         return sandboxRequest("zed/http", "del", [url, options]);
+    },
+
+    startServer: function(id, requestHandlerCommand) {
+        return sandboxRequest("zed/http", "startServer", [id, requestHandlerCommand]);
+    },
+
+    stopServer: function(id) {
+        return sandboxRequest("zed/http", "stopServer", [id]);
     }
 };
